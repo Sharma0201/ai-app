dd763376e03fb5cf3af50c5dc1666d571f67d38f
shiva <sharmashiva0201@gmail.com>
2022-01-29 14:14:20 +0530

    add tensorflow dependency

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='app',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'flask', 'transformers',
+        'flask', 'transformers', 'tensorflow',
     ], )
